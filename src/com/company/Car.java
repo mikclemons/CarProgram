15c0843974197ddf97a61277b80ecbb4cf8b40e2
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-22 14:13:50 -0500

    Create accesor methods for color instance variable.

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -5,5 +5,13 @@ public class Car {
     private String color;
     private String make;
     private String model;
-    private int year; 
+    private int year;
+
+    public void setColor(String carColor){
+        color = carColor;
+    }
+
+    public String getColor(){
+        return color;
+    }
 }
