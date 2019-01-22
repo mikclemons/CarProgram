fbf978dad73bbdeebb4dd8768ada4d32bfe34e07
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-22 14:30:14 -0500

    Use accessor methods to set each instance variable.

--- src/com/company/CarDemo.java
+++ src/com/company/CarDemo.java
@@ -4,6 +4,13 @@ public class CarDemo {
 
     public static void main(String[] args) {
 
-        Car myCar = new Car(); 
+        Car myCar = new Car();
+
+        myCar.setColor("Grey");
+        myCar.setMake("Ford");
+        myCar.setModel("Focus SE");
+        myCar.setYear(2014);
+
+        
     }
 }
